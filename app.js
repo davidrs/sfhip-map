e0a8e7303bc6e40ad867f86dbe7937b6ba7d3ebe
davidrs <drustsmith@drustsmith-macbookpro.roam.corp.google.com>
2016-01-10 15:52:17 -0800

    Add hilighting to tabs

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ var start = function(){
 	setupMap();
 };
 
+
 var setupUiListeners = function(){
 	$(".map-type").click(function(){
 		changeView($(this).attr('id'));
 		hideStores();
+		$(".tab").removeClass("active");
+		$(this).addClass("active");
 	});
 	$("#onSiteStores").click(function(){
 		changeView("onSite");
 		showStores();
+		$(".tab").removeClass("active");
+		$(this).addClass("active");
 	});
-
 };
 
 start();
